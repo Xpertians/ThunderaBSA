33c9e693ced7fdaba65235d1a3f93f806142ea43
oscarvalenzuelab <oscar.valenzuela.b@gmail.com>
2022-05-24 14:53:19 -0700

    Implementing pseudo strings extract

--- thundera/Scanner.py
+++ thundera/Scanner.py
@@ -1,4 +1,5 @@
 import os
+import re
 import time
 import magic
 import mimetypes
@@ -7,10 +8,12 @@ import tarfile
 import gzip
 import csv
 import sys
+import mmap
+import string
 from slugify import slugify
 from zipfile import ZipFile
 from pathlib import Path
-
+from concurrent.futures import ThreadPoolExecutor
 
 class Scanner:
 
@@ -18,21 +21,24 @@ class Scanner:
         self.filelist = filelist
         self.exfilelist = []
         self.enumerate_files(filelist)
-        print(self.filelist)
-        print(self.exfilelist)
+        # print("filelist", self.filelist)
+        # print("exfilelist", self.exfilelist)
+        for filepath in self.filelist:
+            print("file:", filepath)
+            self.process_file(filepath)
 
     def enumerate_files(self, filelist):
+        sub_flist = []
         for file in filelist:
             # exclude symlinks
             if os.path.islink(file):
                 self.exfilelist.append(file)
                 self.filelist.remove(file)
             else:
-                # Quick check for archive file
-                # Needs improvement for anidaded zip files
                 mime = magic.Magic(mime=True)
                 filetype = mime.from_file(file)
                 if self.is_archive(filetype):
+                    looping = True
                     with ZipFile(file, 'r') as zip_ref:
                         zip_dir = os.path.splitext(file)[0]
                         zip_ref.extractall(zip_dir)
@@ -40,8 +46,37 @@ class Scanner:
                         for cdp, csb, cfs in os.walk(zip_dir):
                             for aFile in cfs:
                                 file_path = str(os.path.join(cdp, aFile))
+                                filetype = mime.from_file(file_path)
                                 self.filelist.append(file_path)
-            # check subfolders for zip files
+                                if self.is_archive(filetype):
+                                    sub_flist.append(file_path)
+                                    self.enumerate_files(sub_flist)
+
+    def process_file(self, filepath):
+        print("proc:", filepath)
+        try:
+            fd = open(filepath, "rb")
+            data = fd.read().decode("utf-8", "ignore")
+            fd.close()
+        except:
+            print("There was an error opening the file:\n")
+            print(filepath)
+
+        count = 0
+        window = 5
+        charslist = []
+        printable = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_/\.,1234567890!@#$%^&*(){}[]"
+        for character in data:
+            if character in printable:
+                charslist.append(character)
+                count += 1
+            else:
+                if count >= window:
+                    print(''.join(charslist[-count:]))
+                    count = 0
+        if count >= window:
+            print(''.join(charslist[-count:]))
+
 
     def is_archive(self, file_type):
         # This function needs improvement
